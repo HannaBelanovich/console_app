84f02523adf139527d59edcb84f73ca3b1acc81d
Hanna <Hanna.belanovich@gmail.com>
2022-09-12 11:17:38 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+"use strict";
+
+let number = 4.6;
+
+console.log('string' * 9);
+
+const person = 'Anna';
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+};
+
+console.log(obj.name);
+
+let arr = ['plum.png', 'orange.png', 'apple.png'];
+console.log([1]);
